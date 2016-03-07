93819a879300c215fcc6cf8b23f09e9e2067900f
Eric Pauley <eric@pauley.me>
2016-03-07 16:17:38 -0500

    Output fast in proper format

--- fastq2fasta/fastq2fasta.go
+++ fastq2fasta/fastq2fasta.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 )
 
@@ -14,12 +13,14 @@ func main() {
 		line := input.Bytes()
 		if line[0] == '>' || line[0] == '@' {
 			line[0] = '>'
-			fmt.Fprintln(output, line)
+			output.Write(line)
+			output.WriteRune('\n')
 			push = true
 		} else if line[0] == '+' {
 			push = false
 		} else if push {
-			fmt.Fprintln(output, line)
+			output.Write(line)
+			output.WriteRune('\n')
 		}
 	}
 }
